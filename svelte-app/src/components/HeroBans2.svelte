e2fb378733e43f2a115af5358006f6e064d89ee2
JEO_CARLO LUBAO <jeocarlolubao@gmail.com>
2023-12-31 16:15:38 +0800

    updated hero ban animation
    added a forward slash when a rendered hero appears

--- svelte-app/src/components/HeroBans2.svelte
+++ svelte-app/src/components/HeroBans2.svelte
@@ -25,22 +25,55 @@
       src="portraits/black_image.png"
       alt=""
     />
-    <div class="absolute inset-0 bg-red-600 opacity-60"></div>
+    <div class="absolute inset-0 bg-red-950 opacity-60"></div>
   </div>
 {/if}
 
 {#if hero_name != "none" && hero_name != "banning"}
-  <div class="relative w-full h-full bg-red-600 animate-fade-in">
+  <div class=" relative w-full h-full bg-red-600 animate-fade-in">
     <img
-      class="h-[55px] w-full grayscale"
+      class="  h-[55px] w-full grayscale contrast-125"
       src="portraits/{hero_name}.png"
       alt=""
     />
-    <div class="absolute inset-0 bg-red-600 opacity-60"></div>
+    <div class="slash"></div>
+    <div class="inner-shadow absolute inset-0 bg-red-600 opacity-30"></div>
   </div>
 {/if}
 
 <style>
+  .inner-shadow {
+    box-shadow: inset 0 0 24px 10px #000000;
+  }
+
+  .slash::before {
+    content: "";
+    position: absolute;
+    top: -28px;
+    right: 48px;
+
+    height: 110px;
+    width: 4px;
+    background: red;
+    transform: rotate(65deg);
+    animation: slash-animation 5s forwards;
+  }
+
+  @keyframes slash-animation {
+    0% {
+      opacity: 0;
+      transform: rotate(65deg);
+    }
+    50% {
+      opacity: 1;
+      transform: rotate(65deg);
+    }
+    100% {
+      opacity: 0;
+      transform: rotate(65deg);
+    }
+  }
+
   /* Define the pulse animation */
   @keyframes pulse {
     0% {
