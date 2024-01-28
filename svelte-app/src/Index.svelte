b127bdfb49fd39ca478e705b4a8ce9d5a24fb8e2
JEO_CARLO LUBAO <jeocarlolubao@gmail.com>
2024-01-28 13:00:00 +0800

    Update index.html and Index.svelte with DOTA 2 GSI Overlay

--- svelte-app/src/Index.svelte
+++ svelte-app/src/Index.svelte
@@ -1,3 +1,26 @@
-<a href="#/draft">draft overlay</a>
-<br />
-<a href="#/scoreboard">scoreboard</a>
+<div class="p-5">
+  <a href="#/draft" class="no-underline">
+    <div
+      class="bg-slate-700 w-80 p-4 m-4 text-white text-font hover:scale-105 transition ease-in-out hover:bg-slate-800"
+    >
+      DRAFT OVERLAY
+    </div>
+  </a>
+  <br />
+  <a href="#/scoreboard" class="no-underline">
+    <div
+      class="bg-slate-700 w-80 p-4 m-4 text-white text-font hover:scale-105 transition ease-in-out hover:bg-slate-800"
+    >
+      SCOREBOARD (EXPERIMENTAL)
+    </div>
+  </a>
+</div>
+
+<style>
+  .text-font {
+    font-family: "Radiance_regular", sans-serif;
+  }
+  .no-underline {
+    text-decoration: none;
+  }
+</style>
